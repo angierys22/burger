9e5397177a4cba452afad27c5ae98fa0ae60dfd2
Angelica Reyes <areyes1293@gmail.com>
2020-11-19 23:05:47 -0500

    updated config

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: process.env.LOCAL_MYSQL_PASSWORD,
   database: "burgers_db"
 });
 
